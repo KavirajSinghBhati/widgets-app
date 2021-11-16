858b38a9c011ca774cbc4be6ccfa66ade1c1f180
Kaviraj <ksb1910.07@gmail.com>
2021-11-16 23:20:28 +0530

    widgets app done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
-
 import Translate from "./components/Translate";
+
+import Header from "./components/Header";
+import Route from "./components/Route";
 
 const items = [
   {
@@ -35,34 +37,28 @@ const options = [
   },
 ];
 
-const showAccordian = () => {
-  if (window.location.pathname === "/") {
-    return <Accordion items={items} />;
-  }
-};
-
-const showList = () => {
-  if (window.location.pathname === "/list") {
-    return <Search />;
-  }
-};
-
-const showDropdown = () => {
-  if (window.location.pathname === "/dropdown") {
-    return <Dropdown />;
-  }
-};
-
-const showTranslate = () => {
-  if (window.location.pathname === "/translate") {
-    return <Translate />;
-  }
-};
-
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {showAccordian()} {showList()} {showDropdown()} {showTranslate()}
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
     </div>
   );
 };
